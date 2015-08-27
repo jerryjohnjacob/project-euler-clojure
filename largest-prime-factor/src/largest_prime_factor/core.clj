32f8d712e7750cffbb37ab5b82932968c577406f
jerry <jerry.splat@gmail.com>
2015-08-27 00:59:55 +0000

    Solve largest prime factor problem
    - Highly inefficient solution

---
+++ largest-prime-factor/src/largest_prime_factor/core.clj
@@ -0,0 +1,52 @@
+(ns largest-prime-factor.core
+  (:gen-class))
+
+(def number 600851475143)
+
+(defn is-factor? [main-number number-to-check]
+  (== 0 (rem main-number number-to-check))
+)
+
+(defn round-down [n]
+  (Math/floor n)
+)
+
+(defn list-of-factors [n]
+  (for [x (range 1 (+ (round-down (/ n 2)) 1))
+        :let [y x]
+        :when (is-factor? n y)]
+    y)
+)
+
+(defn number-of-factors [n]
+  (count (list-of-factors n))
+  )
+
+(defn is-prime? [n]
+  (if (== (number-of-factors n) 1)
+    true
+    false
+    )
+  )
+
+(defn is-prime-factor? [main-number number-to-check]
+  (and (is-factor? main-number number-to-check) (is-prime? number-to-check))
+)
+
+(defn get-prime-factors [n list-of-factors]
+  (if (== 1 n)
+    list-of-factors
+    (get-prime-factors (- n 1)
+                       (into list-of-factors (if (is-prime-factor? number n)
+                                               [n]
+                                               []
+                                               )
+                             )
+                       )
+    )
+  )
+
+(defn -main
+  [& args]
+  (println (apply max (get-prime-factors number [])))
+  )
---
+++ largest-prime-factor/test/largest_prime_factor/core_test.clj
@@ -0,0 +1,7 @@
+(ns largest-prime-factor.core-test
+  (:require [clojure.test :refer :all]
+            [largest-prime-factor.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
